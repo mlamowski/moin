47bd8405d9df7cec7f3c3826a69598bed7ca2cf1
mlamowski <82083936+mlamowski@users.noreply.github.com>
2022-11-15 21:14:07 +0100

    projekte + build

--- src/pages/components/Projects.js
+++ src/pages/components/Projects.js
@@ -6,9 +6,13 @@ import Typo_Projects from './Typo_Projects'
 import Arrow_Left from './Arrow_Left'
 import Arrow_Right from './Arrow_Right'
 import Project from './Project'
-import example from './../../img/Screenshot_3.png'
-import example2 from './../../img/Screenshot_1.png'
-import michelle from './../../img/frauenkopf.jpg';
+import alraunenSimulator from './../../img/01_Alraunen_Simulator.png'
+import bearzooka from './../../img/02_Bearzooka.png'
+import pullTheTrigger from './../../img/03_Pull_the_Trigger.png';
+import cameraProjekt01 from './../../img/04_CGA_01.png';
+import cameraProjekt02 from './../../img/04_CGA_02.png';
+import moodium from './../../img/05_Moodium.png';
+
 
 
 import { useState, useEffect } from 'react';
@@ -48,9 +52,12 @@ const Projects = () => {
   }
 
   const projectList = [
-    {"description": "Panda Game", "image": example, "text": "Im vierten Semester ist im Kurs Media Game Design ein Couch-Game entstanden: Bearzooka. "},
-    {"description": "Alraunen Simulator", "image": example2, "text": "Von der Ideenfindung bis hin zur Planung übernehme ich alles gerne."},
-    {"description": "Moodium", "image": michelle, "text": "Von der Ideenfindung bis hin zur Planung übernehme ich alles gerne."},
+    {"description": "Bearzooka", "image": bearzooka, "text": "Bearzooka ist ein Game, welches lokal mit bis zu vier Freunden spielbar ist. Durch Power-Ups und Rubberbanding ist der Spielspaß garantiert. Das Spiel wurde mit Unity entwickelt."},
+    {"description": "Alraunen Simulator", "image": alraunenSimulator, "text": "Mithilfe des Alraunen Simulators kann der User die magische Welt von Harry Potter in einem virtuellen Raum betreten werden. Dabei soll vermittelt werden, die die Zucht einer Alraune erfolgt. Durch direkte Interaktion und Feedback macht dieses Erlebnis besonders viel spaß. Der Simulator wurde mit Unity entwickelt."},
+    {"description": "Camera Project 01", "image": cameraProjekt01, "text": "In diesem selbstentwickelten virtuellen Raum kann der User sich frei bewegen und mit verschiedenen Objekten interagieren. Dabei können eigene Fotos aufgenommen werden. Die Website ist mit Three.JS entstanden."},
+    {"description": "Camera Project 02", "image": cameraProjekt02, "text": "In diesem selbstentwickelten virtuellen Raum kann der User sich frei bewegen und mit verschiedenen Objekten interagieren. Dabei können eigene Fotos aufgenommen werden. Die Kamera-Vorlage wurde mit Blender erstellt. Die zweite Kamera, sowie der virtuelle Raum in dem diese steht wurden in Three.js entwickelt."},
+    {"description": "Moodium", "image": moodium, "text": "Mithilfe des Moodtrackers Moodium lassen sich seine täglichen Stimmungen eintragen und visualisieren. Außerdem lassen sich diese in einem monatlichen Kalender anschauen. Die Website wurde mit Node.js und express entwickelt."},
+    {"description": "Pull the Trigger", "image": pullTheTrigger, "text": "Der Sticker mit dem Motto -Pull the Trigger- lässt sich mit seiner Handykamera einscannen. Der Sticker entwickelt sich zu einer virtuellen Blumenwiese. Probier's mal aus! Der Augmented Reality Effekt wurde in Unity entwickelt, die Blumen in Blender."},
 
   ]
 
@@ -75,7 +82,8 @@ const Projects = () => {
             </button> 
           </div>
           <div className=' w-1/2 sm:w-[280px] '>
-            <p className='relative text-white pt-[30px]'> {projectList[isIndex].text} </p>
+            <h1 className='relative text-white pt-[20px]'> {projectList[isIndex].description}</h1>
+            <p className='relative text-white pt-[10px]'> {projectList[isIndex].text} </p>
           </div>
         </div>
 
