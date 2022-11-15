a022d17ce2bdd89ae13517468f7def6f98b3f419
mlamowski <82083936+mlamowski@users.noreply.github.com>
2022-11-15 22:09:46 +0100

    xx

--- src/pages/components/Startscreen.js
+++ src/pages/components/Startscreen.js
@@ -2,16 +2,37 @@ import React from 'react'
 import Moon from './Moon'
 import Stars from './Stars'
 import Wave1 from './Wave1'
+import { useState, useEffect } from 'react';
+
+
+
+
 
 const Startscreen = () => {
+  const [isNight, setNight] = useState(false);
+
+  function getRealTime() {
+    const locale = 'en';
+    const today = new Date(); // Save the current date to be able to trigger an update
+    const hour = today.getHours();
+    if(hour > 18 || hour < 4) {
+      setNight(true);
+    } else {
+      setNight(false);
+    }
+  }
+
   return (
-    <div className='bg-gradient-to-tr from-[#33061e] to-[#121b2a]  relative -z-10 my-[-1px] '>
+    <div 
+    onLoad={() => {
+      getRealTime();
+  }}
+    className='bg-gradient-to-tr from-[#33061e] to-[#121b2a]  relative -z-10 my-[-1px] '>
         <div className='h-60v sm:h-45v md:h-30v'>
-            <Moon className=" "></Moon>
+            <Moon></Moon>
             <Stars></Stars>
             <Stars></Stars>
 
-            {/* Stars neu machen */}
 
         </div>
         <Wave1></Wave1>
